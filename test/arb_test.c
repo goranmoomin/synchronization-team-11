651ca9fab114f4d2c675f0672cea5e3358e84ace
WallabyG <ycm1366@gmail.com>
2022-04-06 20:35:16 +0900

    Update test programs

--- test/arb_test.c
+++ test/arb_test.c
@@ -4,6 +4,10 @@
 
 int main(int argc, char **argv)
 {
+	if (argc != 4) {
+		fprintf(stderr, "Usage: %s [low] [high] [type]\n", argv[0]);
+		return 1;
+	}
 	long id = syscall(295, atoi(argv[1]), atoi(argv[2]), atoi(argv[3]));
 	printf("Lock %ld acquired!\n", id);
 
